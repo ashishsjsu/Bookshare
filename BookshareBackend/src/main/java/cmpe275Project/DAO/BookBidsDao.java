a1e5508ef2c4b4a4bfc1dee06a197012ae6b30f4
Anurag Bhardwaj <anurag.wizard@gmail.com>
2015-05-11 01:07:07 -0700

    email change

--- BookshareBackend/src/main/java/cmpe275Project/DAO/BookBidsDao.java
+++ BookshareBackend/src/main/java/cmpe275Project/DAO/BookBidsDao.java
@@ -8,10 +8,11 @@ import cmpe275Project.Model.RentOrBuy;
 public interface BookBidsDao {
 	
 	public void addBid(BookBids bookBids);
-	public List<BookBids> listBids(int id);
+	public List<BookBids> listBids(String email);
 	public boolean bidIdExist(Integer bidId);
 	public BookBids getBid(Integer bidId);
 	public RentOrBuy getRentOrBuyRecord(Integer bookId);
 	
 }
+
 
